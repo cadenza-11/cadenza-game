6b7cdbeed20b7022cb2f4e6dfdfda26120642296
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-09-10 17:30:14 -0700

    Add empty input system

---
+++ Assets/Scripts/Systems/Input/InputSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Cadenza
+{
+    public class InputSystem : ApplicationSystem
+    {
+        public override void OnInitialize()
+        {
+            Debug.Log("Input system initialized.");
+        }
+    }
+}
