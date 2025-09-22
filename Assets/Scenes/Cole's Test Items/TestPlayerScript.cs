180df309636adfed662cb900aaa548f6bf1cbbea
NyarlatMkII <coleminer273@gmail.com>
2025-09-22 15:06:54 -0700

    Player Animation and Rework
    -Added animations to player
    -Reworked controller to no longer use raycasting
    -Still don't really get the character controller module, so currently not using that

--- Assets/Scenes/Cole's Test Items/TestPlayerScript.cs
+++ Assets/Scenes/Cole's Test Items/TestPlayerScript.cs
@@ -5,57 +5,57 @@ public class TestPlayerScript : MonoBehaviour
 {
     //This is just taken fron the tutorial, can be adapted later
     public float speed;
-    public float groundDist;
 
     public LayerMask floorLayer;
     public Rigidbody rb;
     public SpriteRenderer sr;
-    public InputAction pc;
+    public InputAction moveP, jumpP;
+    public Animator anim;
+
+    public bool isMove;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Find player Rigidbody
-        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     private void OnEnable()
     {
-        pc.Enable();
+        moveP.Enable();
     }
 
     private void OnDisable()
     {
-        pc.Disable();
+        moveP.Disable();
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //Shoot a line down to find the terrain, then set height just above terrain
-        RaycastHit hit;
-        Vector3 castPos = transform.position;
-        castPos.y += 1;
-        if(Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, floorLayer))
-        {
-            if(hit.collider != null)
-            {
-                Vector3 movePos = transform.position;
-                movePos.y = hit.point.y + groundDist;
-                transform.position = movePos;
-            }
-        }
 
         //Move player and flip sprite
-        Vector3 moveDir = pc.ReadValue<Vector3>();
+        Vector3 moveDir = moveP.ReadValue<Vector3>();
         rb.linearVelocity = moveDir * speed;
 
-        /*if (x != 0 &&  x < 0)
+        if (moveDir.x != 0 && moveDir.x < 0)
         {
             sr.flipX = true;
+            isMove = true;
         }
-        else if (x != 0 && x > 0)
+        else if (moveDir.x != 0 && moveDir.x > 0)
         {
             sr.flipX = false;
+            isMove = true;
         }
-        */
+        else if (Mathf.Abs(moveDir.z) > 0)
+        {
+            isMove = true;
+        }
+        else if (moveDir.x == 0)
+        {
+            isMove = false;
+        }
+
+        anim.SetBool("IsMove", isMove);
     }
 }
