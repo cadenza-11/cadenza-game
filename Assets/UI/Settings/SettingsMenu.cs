4ca4212c1e74c3bac6d459e3bd1e0699d9ec1e73
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-11-28 18:20:20 -0800

    Improve settings menu
    - Add tabs for each setting group

--- Assets/UI/Settings/SettingsMenu.cs
+++ Assets/UI/Settings/SettingsMenu.cs
@@ -9,7 +9,9 @@ namespace Cadenza
         [SerializeField] private UIDocument uiDocument;
 
         private VisualElement blinker;
-        private SliderInt sliderLatency;
+        private Button[] tabButtons;
+        private VisualElement[] tabViews;
+        private int tabIndex = -1;
 
         #region System Events
         public override void OnInitialize()
@@ -18,14 +20,39 @@ namespace Cadenza
             this.root = (TemplateContainer)this.uiDocument.rootVisualElement;
             this.root.style.display = DisplayStyle.None;
 
-            Button buttonBack = this.root.Q<Button>("b_Back");
-            buttonBack.clicked += this.Hide;
+            // Configure tab buttons.
+            var tabButtons = this.root.Q<VisualElement>("tab-buttons");
+            this.tabButtons = new Button[tabButtons.childCount];
+            for (int i = 0; i < tabButtons.childCount; i++)
+            {
+                int index = i; // cache loop index for closure
+                this.tabButtons[i] = tabButtons.ElementAt(i) as Button;
+                this.tabButtons[i].clicked += () => this.SwitchToTab(index);
+            }
 
-            this.sliderLatency = this.root.Q<SliderInt>("slider_Latency");
-            this.sliderLatency.RegisterValueChangedCallback(this.OnLatency);
+            // Configure tab views.
+            var tabViews = this.root.Q<VisualElement>("tab-views");
+            this.tabViews = new VisualElement[tabViews.childCount];
+            for (int i = 0; i < tabViews.childCount; i++)
+            {
+                this.tabViews[i] = tabViews.ElementAt(i);
+                this.tabViews[i].style.display = DisplayStyle.None;
+            }
+
+            // Configure back button.
+            this.root.Q<Button>("b_Back").clicked += this.Hide;
+
+            // Configure general.
+            this.root.Q<Button>("b_DeleteSaveData").clicked += () => SaveSystem.DeletePreviousRuns();
+
+            // Configure calibration.
+            var latencySlider = this.root.Q<SliderInt>("slider_Latency");
+            latencySlider.RegisterValueChangedCallback(evt => BeatSystem.SetOffset(evt.newValue));
+            this.root.Q<Button>("b_SaveCalibration").clicked += () => BeatSystem.SetOffset(latencySlider.value);
             this.blinker = this.root.Q<VisualElement>("icon_Blinker");
 
-            this.root.style.display = DisplayStyle.None;
+            this.SwitchToTab(0);
+            this.Hide();
         }
 
         public override void Show()
@@ -33,8 +60,6 @@ namespace Cadenza
             base.Show();
             BeatSystem.BeatPlayed += () => this.blinker.ToggleInClassList("blink");
             this.root.style.display = DisplayStyle.Flex;
-
-            Debug.LogWarning("Player not being checked. Any player can use settings");
         }
 
         public override void Hide()
@@ -47,13 +72,23 @@ namespace Cadenza
         #endregion
         #region Navigation Events
 
-        private void OnLatency(ChangeEvent<int> evt)
+        private void SwitchToTab(int tabIndex)
         {
-            BeatSystem.SetOffset(evt.newValue);
+            if (tabIndex < 0 || tabIndex >= this.tabViews.Length)
+                return;
+
+            if (this.tabIndex >= 0 && this.tabIndex < this.tabViews.Length)
+            {
+                this.tabViews[this.tabIndex].style.display = DisplayStyle.None;
+                this.tabButtons[this.tabIndex].ToggleInClassList("selected");
+            }
+
+            this.tabIndex = tabIndex;
+            this.tabViews[this.tabIndex].style.display = DisplayStyle.Flex;
+            this.tabButtons[this.tabIndex].ToggleInClassList("selected");
         }
 
         #endregion
-
         #region Private Functions
 
         // Container updates
