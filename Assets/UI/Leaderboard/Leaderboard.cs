e9b0ad0625b66dbb477857be4f7893317b073e17
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-11-30 23:07:51 -0800

    Populate leaderboard UI
    - Leaderboard in backstage scene uses previous runs to display highest scores
    - Fix ScoreSystem not having any score values

--- Assets/UI/Leaderboard/Leaderboard.cs
+++ Assets/UI/Leaderboard/Leaderboard.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Cadenza;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,10 +7,13 @@ using UnityEngine.UIElements;
 [RequireComponent(typeof(Collider), typeof(UIDocument))]
 public class Leaderboard : MonoBehaviour
 {
+    [SerializeField] VisualTreeAsset resultLineAsset;
+
     private UIDocument uiDocument;
     private VisualElement root;
     private Button exitButton;
     private Player player;
+    private Results[] results;
 
     void Start()
     {
@@ -17,9 +21,25 @@ public class Leaderboard : MonoBehaviour
         this.root = this.uiDocument.rootVisualElement;
         this.root.style.display = DisplayStyle.None;
 
+        // Configure exit button.
         this.exitButton = this.root.Q<Button>("b_Exit");
         this.exitButton.clicked += () => this.Hide();
         this.exitButton.RegisterCallback<NavigationSubmitEvent>(_ => this.Hide());
+
+        // Populate leaderboard UI.
+        SaveSystem.GetPreviousRuns(out this.results);
+        this.results = this.results
+            .OrderByDescending(r => r.TeamResults.ScoreTotal)
+            .ToArray();
+
+        var resultsElement = this.root.Q<VisualElement>("results");
+        foreach (var result in this.results)
+        {
+            string teamName = string.IsNullOrEmpty(result.TeamName) ? "Unnamed Team" : result.TeamName;
+            var resultLine = this.resultLineAsset.CloneTree();
+            resultLine.Q<Label>().text = $"{teamName} ... {result.TeamResults.ScoreTotal}";
+            resultsElement.Add(resultLine);
+        }
     }
 
     void OnTriggerEnter(Collider collider)
