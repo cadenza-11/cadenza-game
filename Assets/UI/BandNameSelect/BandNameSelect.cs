cf8997b0176cbf29fcb9fa6b59fe32f8e8568c02
Nicky <157673541+ohnonick@users.noreply.github.com>
2025-12-08 03:27:39 -0800

    Band Name Generator+

--- Assets/UI/BandNameSelect/BandNameSelect.cs
+++ Assets/UI/BandNameSelect/BandNameSelect.cs
@@ -7,10 +7,16 @@ namespace Cadenza
     {
 
         [SerializeField] private UIDocument uiDocument;
+        [SerializeField] private TextAsset articlesFile;
+        [SerializeField] private TextAsset adjectivesFile;
+        [SerializeField] private TextAsset nounsFile;
 
         private Button randomizeName;
         private Button setName;
         private TextField bandNameField;
+        private string[] articles;
+        private string[] adjectives;
+        private string[] nouns;
 
         #region System Events
         public override void OnInitialize()
@@ -26,14 +32,19 @@ namespace Cadenza
             this.setName.clicked += this.OnSetName;
             this.bandNameField = this.root.Q<TextField>("field_BandName");
             this.Hide();
+
+            // Get wordlists
+            this.articles = this.articlesFile.text.Split('\n');
+            this.adjectives = this.adjectivesFile.text.Split('\n');
+            this.nouns = this.nounsFile.text.Split('\n');
         }
 
         public override void Show()
         {
             base.Show();
             PlayerSystem.TryGetPlayerByID(0, out Player player);
-            if (player != null)
-                InputSystem.EnableSinglePlayerInput(player);
+            // if (player != null)
+            //     InputSystem.EnableSinglePlayerInput(player);
             this.root.style.display = DisplayStyle.Flex;
         }
 
@@ -49,8 +60,15 @@ namespace Cadenza
 
         private void OnRandomizeName()
         {
-            Debug.LogWarning("No wordlist implemented. Default name 'The Loud Pandas' used.");
-            this.bandNameField.value = "The Loud Pandas";
+            string bandName = "";
+            int oneIndex = Random.Range(-1, this.articles.Length);
+            int threeIndex = Random.Range(-1, this.nouns.Length);
+            bandName += oneIndex == -1 ? "" : (this.articles[oneIndex] + " ");
+            bandName += this.adjectives[Random.Range(0, this.adjectives.Length)];
+            bandName += threeIndex == -1 ? "" : (" " + this.nouns[threeIndex]);
+            bandName = bandName.Replace("\r", "");
+            Debug.Log($"Generated name: {bandName}");
+            this.bandNameField.value = bandName;
         }
 
         private void OnSetName()
