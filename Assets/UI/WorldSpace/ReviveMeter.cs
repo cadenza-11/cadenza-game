e12dce6bf9672229db0f655ef2e9862150de5c5f
Nicky <157673541+ohnonick@users.noreply.github.com>
2026-02-27 16:30:05 -0800

    Create ReviveMeter
    - UI side stuff

---
+++ Assets/UI/WorldSpace/ReviveMeter.cs
@@ -0,0 +1,41 @@
+using Cadenza;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ReviveMeter : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDocument;
+    private ProgressBar reviveMeter;
+    private InputHint inputHint;
+
+    void Start()
+    {
+        this.reviveMeter = this.uiDocument.rootVisualElement.Q<ProgressBar>();
+        this.inputHint = this.uiDocument.rootVisualElement.Q<InputHint>();
+    }
+
+    public void SetInputHint(ControllerType controller)
+    {
+        this.inputHint.ShownControls = controller;
+    }
+
+    public void SetThreshold(float threshold)
+    {
+        this.reviveMeter.highValue = threshold;
+    }
+
+    public void SetRevive(float revive)
+    {
+        this.reviveMeter.value = Mathf.Min(revive, this.reviveMeter.highValue);
+    }
+
+    public void Show()
+    {
+        this.uiDocument.rootVisualElement.style.display = DisplayStyle.Flex;
+    }
+
+    public void Hide()
+    {
+        this.uiDocument.rootVisualElement.style.display = DisplayStyle.None;
+    }
+}
