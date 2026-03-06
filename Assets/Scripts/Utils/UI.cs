86d865c99cd11c33ba95674618bd33d9da1fb4b7
Marlo Ongkingco <moon.ongkingco@gmail.com>
2026-03-05 16:06:00 -0800

    Add combat countdown UI

--- Assets/Scripts/Utils/UI.cs
+++ Assets/Scripts/Utils/UI.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
@@ -58,6 +59,41 @@ namespace Cadenza.Utils
         private static string Plural(double value)
         {
             return (int)value == 1 ? "" : "s";
+        }
+
+        public static void PunchAndShake(this VisualElement element, float duration)
+        {
+            Vector3 baseScale = Vector3.one;
+            Vector3 baseOffset = Vector3.zero;
+
+            Sequence seq = DOTween.Sequence();
+
+            seq.Join(
+                DOTween.Punch(
+                    getter: () => baseScale,
+                    setter: v => { element.style.scale = new Scale(v); },
+                    direction: new Vector3(0.3f, 0.3f, 0),
+                    duration: duration,
+                    vibrato: 10,
+                    elasticity: 1
+                )
+            );
+            seq.Join(
+                DOTween.Shake(
+                    getter: () => baseOffset,
+                    setter: v => { element.style.translate = new Translate(v.x, v.y, 0); },
+                    duration: duration,
+                    strength: new Vector3(8f, 8f, 0),
+                    vibrato: 20,
+                    randomness: 90
+                )
+            );
+
+            seq.OnComplete(() =>
+            {
+                element.style.scale = new Scale(Vector3.one);
+                element.style.translate = new Translate(0, 0, 0);
+            });
         }
     }
 }
--- Assets/UI/Countdown/Countdown.cs
+++ Assets/UI/Countdown/Countdown.cs
@@ -1,22 +1,82 @@
-using UnityEngine;
+using Cadenza.Utils;
 using UnityEngine.UIElements;
 
 namespace Cadenza
 {
     public class Countdown : UIPanel
     {
+        private static readonly string[] CountdownText = { "3", "2", "1", "GO!" };
+
+        private Label countdownLabel;
+        private int countdownIndex;
+        private bool isCountdownActive;
+
         public override void OnInitialize()
         {
-            GameManager.CombatStarted += this.OnCombatStarted;
+            this.countdownLabel = this.root.Q<Label>("txt_Countdown");
+            this.Hide();
+
+            GameManager.CombatRequested += this.OnCombatRequested;
             GameManager.CombatStopped += this.OnCombatStopped;
         }
 
-        private void OnCombatStarted()
+        public override void OnApplicationStop()
         {
+            this.StopCountdown();
+
+            GameManager.CombatRequested -= this.OnCombatRequested;
+            GameManager.CombatStopped -= this.OnCombatStopped;
+        }
+
+        public override void OnGameStop()
+        {
+            this.StopCountdown();
+        }
+
+        private void OnCombatRequested()
+        {
+            this.countdownIndex = 0;
+            this.isCountdownActive = true;
+            this.countdownLabel.text = string.Empty;
+            this.Show();
+
+            BeatSystem.BeatPlayed -= this.OnBeatPlayed;
+            BeatSystem.BeatPlayed += this.OnBeatPlayed;
         }
 
         private void OnCombatStopped(GameManager.GameResult result)
         {
+            this.StopCountdown();
+        }
+
+        private void OnBeatPlayed()
+        {
+            if (!this.isCountdownActive)
+                return;
+
+            // Hide on the next beat.
+            if (this.countdownIndex >= CountdownText.Length)
+            {
+                this.StopCountdown();
+                return;
+            }
+
+            this.countdownLabel.text = CountdownText[this.countdownIndex];
+            this.countdownIndex++;
+
+            this.countdownLabel.PunchAndShake(0.2f);
+        }
+
+        private void StopCountdown()
+        {
+            this.isCountdownActive = false;
+            this.countdownIndex = 0;
+
+            if (this.countdownLabel != null)
+                this.countdownLabel.text = string.Empty;
+
+            BeatSystem.BeatPlayed -= this.OnBeatPlayed;
+            this.Hide();
         }
     }
 }
