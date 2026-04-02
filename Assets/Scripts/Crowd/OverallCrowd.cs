225071564d6dc468f19b1ed1dd9e22a51fd50c2d
Nicky <victoriano@nevada.unr.edu>
2026-04-02 01:13:26 -0700

    Creating a crowd
    - No optimization (lol)
    - No effect on players yet
    - Reacts to players entering their space

---
+++ Assets/Scripts/Crowd/OverallCrowd.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Cadenza
+{
+    public class OverallCrowd : MonoBehaviour
+    {
+        [SerializeField] public Sprite HandsDownSprite;
+        [SerializeField] public Sprite HandsUpSprite;
+    }
+}
