a6bcf7d2a3cd0f31852cea6fbc272c7b75b3ad85
Nicky <victoriano@nevada.unr.edu>
2026-04-30 11:46:27 -0700

    Fixing DJ core animations

--- Assets/Scripts/Enemy/DJ/DJCore.cs
+++ Assets/Scripts/Enemy/DJ/DJCore.cs
@@ -14,12 +14,16 @@ namespace Cadenza
         private int currentHealth;
         private bool isDestroyed;
         private Material healthIndicatorOutline;
+        private Vector3 originalScale;
+        private Vector3 originalPosition;
 
         public int Health => this.currentHealth;
         public bool IsDestroyed => this.isDestroyed;
 
         void Start()
         {
+            this.originalScale = this.transform.localScale;
+            this.originalPosition = this.transform.localPosition;
             this.healthIndicatorOutline = this.GetComponent<Renderer>().materials[1];
         }
 
@@ -42,6 +46,8 @@ namespace Cadenza
         public void TakeDamage(int damage)
         {
             this.OnHealthChanged(Mathf.Max(this.currentHealth - damage, 0));
+            this.transform.DOShakePosition(0.2f, strength: new Vector3(0.1f, 0, 0.1f), vibrato:20)
+                .OnComplete(() => this.transform.localPosition = this.originalPosition);
             if (this.currentHealth == 0)
                 this.OnDestroyed();
         }
@@ -57,7 +63,8 @@ namespace Cadenza
             this.isDestroyed = true;
             this.dedicatedSpotlight.DOIntensity(0, 0.5f);
             this.electricity.SetActive(true);
-            this.transform.DOShakePosition(1f, strength: new Vector3(0.2f, 0f, 0.2f), vibrato: 10);
+            this.transform.DOShakePosition(1f, strength: new Vector3(0.2f, 0f, 0.2f), vibrato: 10)
+                .OnComplete(() => this.transform.localPosition = this.originalPosition);
 
             BeatSystem.BeatPlayed -= this.PlayPunch;
         }
@@ -65,7 +72,8 @@ namespace Cadenza
         private void PlayPunch()
         {
             if (this.IsDestroyed) return;
-            this.transform.DOPunchScale(new Vector3(this.punchStrength, 0, this.punchStrength), duration: 0.1f, vibrato: 5, elasticity: 0.5f);
+            this.transform.DOPunchScale(new Vector3(this.punchStrength, 0, this.punchStrength), duration: 0.1f, vibrato: 5, elasticity: 0.5f)
+                .OnComplete(() => this.transform.localScale = this.originalScale);
         }
     }
 }
